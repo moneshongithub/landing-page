e726c107e8c20a324eebca2c3911b6d16491c447
monesh lodhi <monesh.lodhi123@gmail.com>
2023-01-11 02:29:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+gsap.to(".imagecontainer",{
+    ease:Expo.easeInOut,
+    width:"100%",
+    duration:2,
+    stagger:2
+
+})
+gsap.to(".text h1",{
+    ease:Expo.easeInOut,
+    duration:2,
+    stagger:2,
+    top:0
+
+})
+gsap.to(".text h1",{
+    ease:Expo.easeInOut,
+    delay:2,
+    duration:2,
+    stagger:2,
+    top:"-100%"
+
+})
