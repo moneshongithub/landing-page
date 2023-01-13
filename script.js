2f84038b29500917e556e565661c3e83b7d7488f
monesh lodhi <monesh.lodhi123@gmail.com>
2023-01-13 20:04:52 +0530

    infinite loop

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-gsap.to(".imagecontainer",{
+var tl = gsap.timeline({
+    repeat: -1  
+})
+tl
+.to(".imagecontainer",{
     ease:Expo.easeInOut,
     width:"100%",
     duration:2,
     stagger:2
 
-})
-gsap.to(".text h1",{
+}, 'a')
+.to(".text h1",{
     ease:Expo.easeInOut,
     duration:2,
     stagger:2,
     top:0
 
-})
-gsap.to(".text h1",{
+}, 'a')
+.to(".text h1",{
     ease:Expo.easeInOut,
     delay:2,
     duration:2,
     stagger:2,
     top:"-100%"
-
-})
+}, 'a')
